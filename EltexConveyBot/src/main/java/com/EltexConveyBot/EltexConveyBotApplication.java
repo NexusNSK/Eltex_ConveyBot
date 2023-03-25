feaf688a84ffb93b045c23d9f6b3df8f4cac55c4
NexusNSK <john_nexus@icloud.com>
2023-03-26 03:59:46 +0700

    Start
    Implement begining function to bot

---
+++ EltexConveyBot/src/main/java/com/EltexConveyBot/EltexConveyBotApplication.java
@@ -0,0 +1,13 @@
+package com.EltexConveyBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EltexConveyBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EltexConveyBotApplication.class, args);
+	}
+
+}
---
+++ EltexConveyBot/src/main/java/com/EltexConveyBot/Service/TgBot.java
@@ -0,0 +1,68 @@
+package com.EltexConveyBot.Service;
+
+import com.EltexConveyBot.Config.BotConfig;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.concurrent.ExecutionException;
+
+@Component
+public class TgBot extends TelegramLongPollingBot {
+
+    final BotConfig config;
+
+    public TgBot(BotConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+
+            switch (messageText) {
+                case "/start":
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                default:
+                    sendMessage(chatId, "Команда не поддерживается.");
+            }
+        }
+
+    }
+
+    private void startCommandReceived(long chatId, String name) {
+        String answer = "Привет, " + name + ", это бот-помощник на конвейере!\nПока мой функционал сильно ограничен. Разработчик будет постепенно добавлять новые фичи для твоего удобства.";
+        sendMessage(chatId, answer);
+
+    }
+
+    private void sendMessage(long chatId, String textToSend) {
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText(textToSend);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    public String getBotToken() {
+        return config.getBotToken();
+    }
+
+}
